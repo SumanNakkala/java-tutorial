a850cc6b0326264a3a69b16436218b473eb40195
Suman Nakkala <sumankrishna1988@gmail.com>
2020-07-20 08:32:29 +0530

    day 4

--- conditionals/src/com/suman/conditionals/SwitchExample.java
+++ conditionals/src/com/suman/conditionals/SwitchExample.java
@@ -1,8 +1,30 @@
 package com.suman.conditionals;
 
+import java.util.Scanner;
+
 public class SwitchExample {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner((System.in));
+        System.out.print("Enter Choice: ");
+        int choice = scanner.nextInt();
+
+
+        switch( choice )  {
+
+            case  1 :
+                System.out.println("hello");
+                break;
+            case  2 :
+                System.out.println("World");
+                break;
+
+            default:
+                System.out.println(" Please enter an valid choice ( please enter 1 or 2) ");
+
+
+        }
 
 
 
